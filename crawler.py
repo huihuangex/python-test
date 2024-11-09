c61f676abb0c286cc8c4eb4b457b83fd06266872
grey_rabbit <huihuangvx@gmail.com>
2024-11-09 16:08:05 +0800

    123

--- crawler.py
+++ crawler.py
@@ -1,7 +1,10 @@
+# 导入requests库用于发起HTTP请求
 import requests
+# 导入BeautifulSoup用于解析HTML文档
 from bs4 import BeautifulSoup
 
 def web_scraping(url):
+    '''抓取指定 URL 网页的段落文本'''
     # 发送请求，获取 HTML 内容
     response = requests.get(url)
     response.raise_for_status() # 如果请求失败，抛出异常
@@ -22,6 +25,7 @@ url = 'https://www.bilibili.com/video/BV12j41167WZ/?spm_id_from=333.1007.tianma.
 text = web_scraping(url)
 
 with open('output.txt', 'w', encoding='utf-8') as f:
+    # 将抓取到的段落文本写入文件
     for paragraph in text:
         f.write(paragraph + '\n')
 
